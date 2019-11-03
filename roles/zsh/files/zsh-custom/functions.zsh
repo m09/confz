0f9170799b188194dfb340a058e76e9f2205247c
m09 <142691+m09@users.noreply.github.com>
2019-11-03 08:55:28 +0100

    Sync
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- roles/zsh/files/zsh-custom/functions.zsh
+++ roles/zsh/files/zsh-custom/functions.zsh
@@ -1,9 +1,17 @@
 function aziz() {
+    local brightness
     if [[ $# > 0 ]]; then
-        xrandr --output DP-1 --brightness "0.$1"
-        xrandr --output eDP-1 --brightness "0.$1"
+        brightness="0.$1"
     else
-        xrandr --output DP-1 --brightness 1
-        xrandr --output eDP-1 --brightness 1
+        brightness=1
     fi
+    pkill redshift-gtk
+    redshift-gtk -l 47.2173:-1.5534 -r -b "$brightness":"$brightness" & disown
+}
+
+function optimize-png() {
+    local img="$1"
+    pngquant -f "$img" -o "$img"
+    optipng -o7 "$img"
+    zopflipng -ym "$img" "$img"
 }
