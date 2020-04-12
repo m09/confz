d458046eeb14fca00986f6bd60bb8adb5c29ab6e
m09 <142691+m09@users.noreply.github.com>
2020-04-12 13:47:48 +0200

    Use new mechanism to encrypt host file
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- roles/zsh/files/zsh-custom/aliases.zsh
+++ roles/zsh/files/zsh-custom/aliases.zsh
@@ -1,7 +1,3 @@
-alias u='sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremove --purge && sudo apt-get clean'
-alias ansible-playbook='ansible-playbook --vault-id @prompt -K -i hosts'
-alias j=jump
-
 files="$(find "$ZSH_CUSTOM/alias/" -type f -name '*.zsh-alias' -print0 | sort -z)"
 IFS=$'\0' read -r -A files <<< "$files"
 for f in "${files[@]}"; do
